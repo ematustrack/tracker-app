383783cb16128a4c302be93f3a3a656177fbfff6
Farid <farid.abulias@gmail.com>
2017-07-27 16:31:04 -0400

    deploy

--- trackerServer/trackerServer/settings.py
+++ trackerServer/trackerServer/settings.py
@@ -59,7 +59,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,4 +127,9 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static-root')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
--- trackerServer/trackerServer/urls.py
+++ trackerServer/trackerServer/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^server/', include('server.urls'))
+    url(r'^server/', include('server.urls')),
+    url(r'^$', TemplateView.as_view(template_name="ang_home.html"), name='index'),
 ]
